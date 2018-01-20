0b42c56d6e2c067432a73b804036a52230727c08
MulticsYin <multics_luo@163.com>
2018-01-20 23:03:57 +0800

    change

--- devlop.go
+++ devlop.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json;charset=utf-8")
-		w.Write([]byte(`{"code":0,"data":"test data three"}`))
+		w.Write([]byte(`{"code":0,"data":"test data one"}`))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
