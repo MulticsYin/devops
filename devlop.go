1b80db24924662b96226b7085b33717ccaada5fd
MulticsYin <multics.luo@gmail.com>
2018-01-20 09:07:10 +0800

    add http head

--- devlop.go
+++ devlop.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json;charset=utf-8")
 		w.Write([]byte(`{"code":0,"data":"successful"}`))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
