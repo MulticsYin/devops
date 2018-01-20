d75ba5df4486828cb381fea199af4cccb6b977b8
MulticsYin <multics.luo@gmail.com>
2018-01-20 11:13:09 +0800

    add bash file

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo -e "Start deploy projuct"
+killall devops
+cd $GOPATH/src
+
+if [ ! -d $GoPATH/src/devops ]; then
+    echo -e "git clone from https://github.com/MulticsYin/devops.git"
+    git clone https://github.com/MulticsYin/devops.git
+    cd $GOPATH/src/devops
+else
+    echo -e "Find old project, git pull from https://github.com/MulticsYin/devops.git"
+    cd $GOPATH/src/devops
+    git pull
+fi
+
+echo -e "Build project now"
+go build . && ./devops &
+
