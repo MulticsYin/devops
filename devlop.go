45ddda5abf6b0d01097ba513d27ef3189ae2a991
MulticsYin <multics.luo@gmail.com>
2018-01-20 11:14:55 +0800

    deploy test

--- devlop.go
+++ devlop.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json;charset=utf-8")
-		w.Write([]byte(`{"code":0,"data":"successful"}`))
+		w.Write([]byte(`{"code":0,"data":"test data"}`))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
