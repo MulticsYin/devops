47eb0ed04758aea6350b99ec2515245af144fe7b
MulticsYin <multics.luo@gmail.com>
2018-01-22 11:33:11 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo -e "Start deploy projuct"
 killall devops
 cd $GOPATH/src
 
-if [ ! -d "test" ]; then
+if [ ! -d "/home/multics/go/gopath/src/deploy" ]; then
     echo -e "git clone from https://github.com/MulticsYin/devops.git"
     git clone https://github.com/MulticsYin/devops.git
     cd $GOPATH/src/devops
