2295ada1ea5ac26d34c22b3471dfe0ac03ae9980
MulticsYin <multics.luo@gmail.com>
2018-04-03 15:40:04 +0800

    gopher

--- devlop.go
+++ devlop.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json;charset=utf-8")
-		w.Write([]byte(`{"code":0,"data":"Hello Goalng!"}`))
+		w.Write([]byte(`{"code":0,"data":"Hello Gopher ! ! !"}`))
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
